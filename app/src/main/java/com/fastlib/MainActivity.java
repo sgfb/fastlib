07eec3e202f1d57a88e83f022970e230cc90e6fa
上官福抱 <shangguanfb@hzhanghuan.com>
2017-06-25 23:24:22 +0800

    update

--- app/src/main/java/com/fastlib/MainActivity.java
+++ app/src/main/java/com/fastlib/MainActivity.java
@@ -1,6 +1,9 @@
 package com.fastlib;
 
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.util.Pair;
 import android.util.SparseArray;
@@ -20,6 +23,7 @@ import com.fastlib.db.FastDatabase;
 import com.fastlib.net.Request;
 import com.fastlib.net.ResponseStatus;
 import com.fastlib.net.SimpleListener;
+import com.fastlib.utils.N;
 import com.google.gson.Gson;
 
 import java.util.ArrayList;
@@ -27,12 +31,14 @@ import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 /**
  * Created by sgfb on 17/5/10.
  */
 @ContentView(R.layout.act_main)
 public class MainActivity extends FastActivity{
+    public static final int REQ_ENABLE_BLUETOOTH=2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState){
@@ -41,42 +47,53 @@ public class MainActivity extends FastActivity{
     }
     @Bind(R.id.bt)
     private void commit(TextView v){
-        Request request=Request.obtain("get","http://www.chexge.com/api/v1/car/check");
-        request.put("templateName","外观检测");
-        request.setListener(new SimpleListener<String>(){
-
-            @Override
-            public void onResponseListener(Request r, String result) {
-                System.out.println("result:"+result);
+        BluetoothAdapter ba=BluetoothAdapter.getDefaultAdapter();
+        if(ba!=null){
+            if(ba.isEnabled()){
+                useBlueTooth();
             }
-        });
-        net(request);
+            else{
+                Intent intent=new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+                startActivityForResult(intent,REQ_ENABLE_BLUETOOTH);
+            }
+        }
+        else{
+            N.showShort(this,"你没有蓝牙设备");
+        }
+    }
+
+    private void useBlueTooth(){
+        BluetoothAdapter ba=BluetoothAdapter.getDefaultAdapter();
+        Set<BluetoothDevice> set=ba.getBondedDevices();
+        if(set.size()>0){
+            for(BluetoothDevice bd:set){
+                N.showLong(this,bd.getName()+","+bd.getAddress());
+            }
+        }
+        else System.out.println("no bonded device");
     }
 
     @Bind(R.id.bt2)
     private void commit2(){
-        Request request=Request.obtain("get","http://www.chexge.com/api/v1/users/technician?storeId=1&page=1&size=10");
-        request.put("storeId",1);
-        request.put("page",1);
-        request.put("size",10);
-        request.setListener(new SimpleListener<String>(){
 
-            @Override
-            public void onResponseListener(Request r, String result) {
-                System.out.println("result:"+result);
-            }
-        });
-        net(request);
     }
 
     @Bind(R.id.bt3)
     private void commit3(View view){
-        Bean b=new Bean();
-        Gson gson=new Gson();
-        b.id=1;
-        b.content="sgfb";
-        b.url="http:www.baidu.com";
-        System.out.println(gson.toJson(b,Bean.class));
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode,int resultCode,Intent data){
+        super.onActivityResult(requestCode,resultCode,data);
+        if(requestCode==REQ_ENABLE_BLUETOOTH){
+            if(resultCode==RESULT_OK){
+                useBlueTooth();
+            }
+            else if(resultCode==RESULT_CANCELED){
+                N.showShort(this,"用户取消了蓝牙设置或者其他异常导致蓝牙打开失败");
+            }
+        }
     }
 
     @Override
