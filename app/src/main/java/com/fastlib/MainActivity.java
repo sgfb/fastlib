cbc8d9dbab1eadac80db566cc2bccb356f3c4cd6
sgfb <shangguanfb@hzhanghuan.com>
2018-02-05 18:55:44 +0800

    temp

--- app/src/main/java/com/fastlib/MainActivity.java
+++ app/src/main/java/com/fastlib/MainActivity.java
@@ -32,8 +32,6 @@ public class MainActivity extends FastActivity{
     ImageView mImage2;
     @Bind(R.id.image3)
     ImageView mImage3;
-    @Bind(R.id.duration)
-    EditText mDuration;
 
     @Override
     protected void alreadyPrepared(){
@@ -44,23 +42,12 @@ public class MainActivity extends FastActivity{
 
     @Bind(R.id.bt)
     private void commit(){
-        Request request=new Request("http://www.wiboson.com/fz/api/upload");
-        request.put("token","f4f75049e13346178f2ea7f9b225955b");
-        request.put("file",new File(Environment.getExternalStorageDirectory(),"temp.jpg"));
-        request.setListener(new SimpleListener<String>(){
-
-            @Override
-            public void onResponseListener(Request r, String result) {
-                System.out.println("result:"+result);
-            }
-        });
-        net(request);
-//        BitmapRequest br=BitmapRequestEntrance.factory(this)
-//                .bitmapRequestByUrl("https://static.oschina.net/uploads/space/2018/0106/134811_VkaD_347223.jpg")
-//                .setImageView(mImage)
-//                .setStoreStrategy(FastImageConfig.STRATEGY_STORE_SAVE_DISK)
-//                .setSpecifiedStoreFile(new File(Environment.getExternalStorageDirectory(),"temp.jpg"));
-//        FastImage.getInstance().startRequest(br);
+        BitmapRequest br=BitmapRequestEntrance.factory(this)
+                .bitmapRequestByUrl("https://static.oschina.net/uploads/space/2018/0106/134811_VkaD_347223.jpg")
+                .setImageView(mImage)
+                .setStoreStrategy(FastImageConfig.STRATEGY_STORE_SAVE_DISK)
+                .setSpecifiedStoreFile(new File(Environment.getExternalStorageDirectory(),"temp.jpg"));
+        FastImage.getInstance().startRequest(br);
     }
 
     @Bind(R.id.bt2)
@@ -78,14 +65,6 @@ public class MainActivity extends FastActivity{
     private void commit3(){
         BitmapRequest br=BitmapRequestEntrance.factory(this)
                 .bitmapRequestByUrl("http://c.hiphotos.baidu.com/image/pic/item/bd315c6034a85edfef0cf9e940540923dc547573.jpg")
-                .setAnimator(new BitmapRequest.ViewAnimator() {
-                    @Override
-                    public void animator(View v) {
-                        v.setScaleX(0);
-                        v.setScaleY(0);
-                        v.animate().scaleX(1).scaleY(1).setInterpolator(new OvershootInterpolator()).setDuration(Long.parseLong(mDuration.getText().toString()));
-                    }
-                })
                 .setImageView(mImage3);
         FastImage.getInstance().startRequest(br);
     }
