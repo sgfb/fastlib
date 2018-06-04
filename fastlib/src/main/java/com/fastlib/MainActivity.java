2a738710eb9aac60880cfd2bb14938c897393788
看那边 <835855592@qq.com>
2018-06-04 08:48:57 +0800

    Task任务切换时使用线程驱动已防止方法重叠溢出

--- fastlib/src/main/java/com/fastlib/MainActivity.java
+++ fastlib/src/main/java/com/fastlib/MainActivity.java
@@ -1,39 +1,38 @@
 package com.fastlib;
 
-import com.fastlib.R;
+import android.view.View;
+
 import com.fastlib.annotation.Bind;
 import com.fastlib.annotation.ContentView;
 import com.fastlib.app.FastActivity;
+import com.fastlib.app.task.InfiniteAction;
 import com.fastlib.app.task.NoReturnAction;
 import com.fastlib.app.task.Task;
+import com.fastlib.app.task.TaskLauncher;
 import com.fastlib.app.task.ThreadType;
-
-import android.content.Intent;
-import android.view.View;
-
-import android.widget.Button;
 
 @ContentView(R.layout.act_main)
 public class MainActivity extends FastActivity{
+    TaskLauncher mLauncher;
 
 	@Override
 	protected void alreadyPrepared(){
-
+        Runtime.getRuntime().addShutdownHook(new Thread(){
+            @Override
+            public void run() {
+                super.run();
+                System.out.println("shutdown");
+            }
+        });
 	}
 
 	@Bind(R.id.bt)
 	public void onBt(View view){
-		startTask(Task.begin("10")
-				.infinite(new NoReturnAction<String>() {
-					@Override
-					public void executeAdapt(String param) {
-						System.out.println(param);
-					}
-				}, ThreadType.WORK));
+
 	}
 
 	@Bind(R.id.bt2)
 	public void onBt2(View view){
-		
+
 	}
 }
