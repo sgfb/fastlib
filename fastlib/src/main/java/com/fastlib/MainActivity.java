8ca587203c29c61b9c8be455d2a64c8f54822dcd
sgfb <602687446@qq.com>
2019-04-17 18:50:23 +0800

    重构AutoFitGrid

--- fastlib/src/main/java/com/fastlib/MainActivity.java
+++ fastlib/src/main/java/com/fastlib/MainActivity.java
@@ -1,91 +1,30 @@
 package com.fastlib;
 
-import android.content.Intent;
-import android.text.format.Formatter;
 import android.widget.EditText;
 
-import com.fastlib.alpha.ClientRecordActivity;
-import com.fastlib.alpha.ServerMonitorActivity;
 import com.fastlib.annotation.Bind;
 import com.fastlib.annotation.ContentView;
-import com.fastlib.annotation.Event;
 import com.fastlib.app.module.FastActivity;
-import com.fastlib.app.task.ThreadPoolManager;
-import com.fastlib.bean.event.EventDownloading;
-import com.fastlib.net.DefaultDownload;
-import com.fastlib.net.Request;
-import com.fastlib.net.listener.SimpleListener;
-
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
-import java.net.Socket;
-import java.util.Locale;
+import com.fastlib.widget.AutoFitGridView;
 
 @ContentView(R.layout.act_main)
-public class MainActivity extends FastActivity{
+public class MainActivity extends FastActivity {
+    @Bind(R.id.autoFitGrid)
+    AutoFitGridView mAutoFitGrid;
+    @Bind(R.id.text)
+    EditText mText;
 
     @Override
-    public void alreadyPrepared(){
-
+    public void alreadyPrepared() {
     }
 
     @Bind(R.id.bt)
-    private void startServer(){
-        ThreadPoolManager.sSlowPool.execute(new Runnable() {
-            @Override
-            public void run() {
-                try {
-                    File file=new File(getExternalCacheDir(),"tcpTest.rar");
-                    if(!file.exists())
-                        file.createNewFile();
+    private void startServer() {
 
-                    long timer=System.currentTimeMillis();
-                    Socket socket=new Socket("192.168.1.122",2888);
-                    InputStream in=socket.getInputStream();
-                    OutputStream out=new FileOutputStream(file);
-                    byte[] buffer=new byte[4096*2];
-                    int len;
-                    while((len=in.read(buffer))!=-1)
-                        out.write(buffer,0,len);
-                    in.close();
-                    out.close();
-                    socket.close();
-                    System.out.println(String.format(Locale.getDefault(),"consume time:%d",System.currentTimeMillis()-timer));
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
-        });
     }
 
     @Bind(R.id.bt2)
-    private void startClient(){
-        File file=new File(getExternalCacheDir(),"tcpTest.rar");
-        if(!file.exists()) {
-            try {
-                file.createNewFile();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }
-        Request request=new Request("get","http://192.168.1.122:8080/FastProject/upload/BrorrowWalletSourceCode.rar");
-        request.setDownloadable(new DefaultDownload(file));
-        final long timer=System.currentTimeMillis();
-        request.setListener(new SimpleListener<String>(){
+    private void startClient() {
 
-            @Override
-            public void onResponseListener(Request r, String result) {
-                System.out.println(String.format(Locale.getDefault(),"consume time:%d",System.currentTimeMillis()-timer));
-            }
-        });
-        request.start();
-    }
-
-    @Event
-    private void eDownload(EventDownloading eventDownloading){
-        System.out.println("speed:"+Formatter.formatFileSize(this,eventDownloading.getSpeed()));
     }
 }
---
+++ fastlib/src/main/java/com/fastlib/adapter/AutoFitGridAdapter.java
@@ -0,0 +1,69 @@
+package com.fastlib.adapter;
+
+import android.content.Context;
+import android.database.DataSetObservable;
+import android.database.DataSetObserver;
+import android.support.v4.util.Pair;
+import android.view.View;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Create by sgfb on 2019/04/17
+ * E-Mail:602687446@qq.com
+ * 自适应网格基础适配器
+ */
+public abstract class AutoFitGridAdapter<T>{
+    private DataSetObservable mDataSetObservable=new DataSetObservable();
+    protected List<T> mData;
+    protected Context mContext;
+
+    public AutoFitGridAdapter(Context context){
+        mContext=context;
+    }
+
+    public AutoFitGridAdapter(List<T> mData) {
+        this.mData = mData;
+    }
+
+    public abstract Pair<Integer,View> getView(int position, T data);
+
+    public T getItemAtPosition(int position){
+        return mData.get(position);
+    }
+
+    public int getCount(){
+        return mData==null?0:mData.size();
+    }
+
+    public void setData(List<T> data){
+        mData=data;
+        notifyDataSetChanged();
+    }
+
+    public void addData(T data){
+        if(mData==null) mData=new ArrayList<>();
+        mData.add(data);
+        notifyDataSetChanged();
+    }
+
+    public void remove(int index){
+        if(mData!=null){
+            mData.remove(index);
+            notifyDataSetChanged();
+        }
+    }
+
+    public void registerDataSetObserver(DataSetObserver observer){
+        mDataSetObservable.registerObserver(observer);
+    }
+
+    public void unregisterDataSetObserver(DataSetObserver observer){
+        mDataSetObservable.unregisterObserver(observer);
+    }
+
+    private void notifyDataSetChanged(){
+        mDataSetObservable.notifyChanged();
+    }
+}
