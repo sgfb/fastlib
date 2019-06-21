33beab1baeaf6a74214e538d1504048670f8d28f
sgfb <602687446@qq.com>
2019-06-21 19:26:50 +0800

    临时保存

--- fastlib/src/main/java/com/fastlib/MainActivity.java
+++ fastlib/src/main/java/com/fastlib/MainActivity.java
@@ -1,9 +1,11 @@
 package com.fastlib;
 
+import android.Manifest;
 import android.content.Intent;
 import android.support.v4.app.Fragment;
 import android.support.v4.util.Pair;
 import android.support.v4.view.ViewPager;
+import android.view.View;
 import android.webkit.CookieManager;
 import android.webkit.CookieSyncManager;
 import android.webkit.WebView;
@@ -24,37 +26,39 @@ import com.fastlib.net.listener.GlobalListener;
 import com.fastlib.net.listener.SimpleListener;
 import com.fastlib.utils.Utils;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Locale;
 
 @ContentView(R.layout.act_main)
 public class MainActivity extends FastActivity {
 
     @Bind(R.id.bt)
     private void startServer(){
-        Request request=new Request("www.baidu.com","get");
-        request.setListener(new SimpleListener<String>(){
-
-            @Override
-            public void onResponseListener(Request r, String result) {
-                System.out.println("response:"+r.hashCode());
-            }
-        });
-        System.out.println("raw hs:"+request.hashCode());
-        request.put("a","b");
-        System.out.println("start hs:"+request.hashCode());
-        request.start();
+        startActivity(new Intent(this,ServiceActivity.class));
     }
 
     @Bind(R.id.bt2)
     private void bt2(){
+        startActivity(new Intent(this,ClientActivity.class));
+    }
+
+    @Bind(R.id.bt3)
+    private void bt3(){
 
     }
 
     @Override
     public void alreadyPrepared() {
-        NetManager.getInstance().setRootAddress("http://");
+
     }
 }
